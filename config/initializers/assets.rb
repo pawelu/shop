24518d418e973c607be08d133d36f9c26db5d4b6
Paweł Usowski <pawel.usowski@gmail.com>
2016-09-11 01:57:59 +0200

    Add backend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( logo/spree_50.png noimage/*.png )
+Rails.application.config.assets.precompile += %w( jquery-ui/* admin/* credit_cards/credit_card.gif )
