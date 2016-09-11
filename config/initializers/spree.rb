e09f0de3537f1fd16be40c0d8d37a5f19268e3ec
Paweł Usowski <pawel.usowski@gmail.com>
2016-09-11 02:24:48 +0200

    Fix routes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,4 +17,3 @@ end
 
 Spree.user_class = "Spree::User"
 # Spree Configuration
-SESSION_KEY = '_spree_session_id'
--- config/routes.rb
+++ config/routes.rb
@@ -1,75 +1,10 @@
 Rails.application.routes.draw do
-
-  # This line mounts Spree's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
-          # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
-
-Spree::Core::Engine.add_routes do
   get '/forbidden', to: 'home#forbidden', as: :forbidden
   # those routes are needed for mailers
   root to: 'home#index'
   resources :products, only: [:index, :show]
-end
 
-Spree::Core::Engine.add_routes do
+  ##
   namespace :admin, path: Spree.admin_path do
     get '/search/users', to: "search#users", as: :search_users
     get '/search/products', to: "search#products", as: :search_products
@@ -253,11 +188,8 @@ Spree::Core::Engine.add_routes do
   end
 
   get Spree.admin_path, to: 'admin/root#index', as: :admin
-end
+  ####
 
-
-
-Spree::Core::Engine.add_routes do
 
   get '/locale/set', to: 'locale#set'
 
@@ -288,4 +220,5 @@ Spree::Core::Engine.add_routes do
   get '/content/cvv', to: 'content#cvv', as: :cvv
   get '/content/*path', to: 'content#show', as: :content
   get '/cart_link', to: 'store#cart_link', as: :cart_link
+
 end
